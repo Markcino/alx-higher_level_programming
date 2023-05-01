1f96cb49f0d81115d125d20eec45bcea2caa59a6
Markcino <33788083+Markcino@users.noreply.github.com>
2023-05-01 19:39:43 +0000

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Add all arguments to a Python list and save them to a file."""
 import sys
 
@@ -14,3 +13,4 @@ if __name__ == "__main__":
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
+    
