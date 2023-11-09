967b4e05cfd222d0de41ab1b4d8127befc7fad19
Markcino <33788083+Markcino@users.noreply.github.com>
2023-11-09 05:15:50 +0000

    delete

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-let fs = require('fs');
-
-let data = process.argv[3];
-
-fs.writeFile(process.argv[2], data, function (err, data) {
-  if (err) console.log(err);
-});
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const r = require('request');
-const u = 'http://swapi.co/api/films/' + process.argv[2];
-r(u, function (error, response, body) {
-  console.log(JSON.parse(body).title || error);
-});
